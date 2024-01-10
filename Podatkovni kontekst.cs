04d1648494aa7081a6173b6528957351699f8e00
PetarPelmo <148312113+PetarPelmo@users.noreply.github.com>
2024-01-10 08:30:59 +0100

    Add files via upload

---
+++ Podatkovni kontekst.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pracenje_filmskih_ocjena
+{
+    internal class Podatkovni_kontekst
+    {
+        private List<korisnik> korisnici;
+        private List<film> filmovi;
+        private List<ocjena> ocjene;
+
+        public List<korisnik> Korisnici { get {  return korisnici; } }
+        public List<film> Filmovi { get { return filmovi; } }
+        public List<ocjena> Ocjene { get { return ocjene; } }
+    }
+}
